df2cf7c8293b02f677094e4c74f1b138e64ded62
heejinna <heejin.info@gmail.com>
2022-04-13 17:21:05 +0900

    모달 컴포넌트 파일명 변경

---
+++ src/components/modal/cSU_SearchModal.vue
@@ -0,0 +1,83 @@
+<template>
+  <section class="search-modal-area">
+    <input type="text" v-model="coffeeNameVal" placeholder="음료명을 입력하세요">
+    <select placeholder="선택" v-model="coffeeStickerVal">
+      <option value="New">New</option>
+      <option value="Best">Best</option>
+    </select>
+    <div class="btn-container">
+      <button @click="closeModal" class="cancel-btn">취소</button>
+      <cBtn @click="searchCoffee" class="search-btn" btnName="검색"></cBtn>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SearchModal',
+  props: {
+    searchModalShowYn: Boolean
+  },
+  data () {
+    return {
+      coffeeNameVal: '',
+      coffeeStickerVal: ''
+    }
+  },
+  methods: {
+    closeModal () {
+      this.$emit('closeModal')
+    },
+    searchCoffee () {
+      // eslint-disable-next-line no-new-object
+      const param = new Object()
+      if ((this.coffeeNameVal !== undefined && this.coffeeNameVal !== '')) {
+        param.coffeeName = this.coffeeNameVal
+      }
+      if (this.coffeeStickerVal !== undefined && this.coffeeStickerVal !== '') {
+        param.coffeeSticker = this.coffeeStickerVal
+      }
+      alert(JSON.stringify(param))
+      this.$emit('searchCoffee', param)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  input, select {
+    font-family: 'AppleSDGothicNeo', sans-serif;
+    padding: 8px;
+    margin-bottom: 15px;
+    border: none;
+    border-radius: 3px;
+    background-color: #eee;
+    font-weight: 400;
+  }
+  .search-modal-area {
+    background-color: #fff;
+    width: 300px;
+    height: 170px;
+    border: 1px solid #ccc;
+    border-radius: 7px;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+  }
+  .btn-container {
+    display: flex;
+    justify-content: end;
+  }
+  .btn-container button {
+    font-family: 'AppleSDGothicNeo', sans-serif;
+    width: 50px;
+    height: 30px;
+    border-radius: 30px;
+    border: 2px solid #00A862;
+  }
+  .cancel-btn {
+    cursor: pointer;
+    color: #00A862;
+    margin-right: 7px;
+  }
+</style>
--- src/components/modal/cSU_SearchPopup.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section class="search-modal-area">
-    <input type="text" placeholder="음료명을 입력하세요">
-    <select name="label"/>
-  </section>
-</template>
