2a431019422b8ec5422456481540b859cf698014
heejinna <heejin.info@gmail.com>
2022-04-13 17:21:52 +0900

    커스텀 버튼 마크업

---
+++ src/components/layout/cSU_CommonBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="commonBtn">{{ btnName }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    btnName: {}
+  }
+}
+</script>
+
+<style scoped>
+  .commonBtn {
+    font-family: 'AppleSDGothicNeo', sans-serif;
+    font-weight: 400;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 33px;
+    background-color: #00A862;
+    color: #fff;
+    border: none;
+    border-radius: 30px;
+  }
+</style>
