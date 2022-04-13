87c97ef7cc8b1f548b7eeb962b7158ebe5fa60f2
heejinna <heejin.info@gmail.com>
2022-04-13 17:22:14 +0900

    컴포넌트 구조 변경

--- src/pages/pSU_OrderView.vue
+++ src/pages/pSU_OrderView.vue
@@ -1,22 +1,16 @@
 <template>
   <section class="order">
-    <OrderHeader/>
     <OrderList/>
-    <SearchPopup/>
   </section>
 </template>
 
 <script>
-import OrderHeader from '../components/layout/cSU_OrderHeader.vue'
 import OrderList from '../components/order/cSU_OrderList.vue'
-import SearchPopup from '../components/modal/cSU_SearchPopup.vue'
 
 export default {
   name: 'OrderView',
   components: {
-    OrderHeader,
-    OrderList,
-    SearchPopup
+    OrderList
   }
 }
 </script>
