9f5c349543af6f15fb7b4a0deb025113aa1ef0c3
heejinna <heejin.info@gmail.com>
2022-04-18 15:21:17 +0900

    주문 결과창 스타일링

--- src/components/modal/cSU_ResultModal.vue
+++ src/components/modal/cSU_ResultModal.vue
@@ -1,14 +1,18 @@
 <template>
-  <blackBg></blackBg>
+  <blackBg class="black-bg"></blackBg>
   <section class="order-result-container">
-    <h3>주문 결과</h3>
-    <p>{{ detailInfo.coffeeName }}</p>
-    <p>{{ volume }}</p>
-    <p>{{ optionSelected.size }}</p>
-    <p>{{ optionSelected.cup }}</p>
+    <h3 class="result-title">주문 결과</h3>
+    <div class="coffee-info">
+      <p>{{ detailInfo.coffeeName }}</p>
+      <span class="coffee-volume">{{ volume }}</span>
+    </div>
+    <ul class="option-info">
+      <li>{{ optionSelected.size }}</li>
+      <li>{{ optionSelected.cup }}</li>
+    </ul>
     <hr>
-    <p>{{ totalPrice }}원</p>
-    <cBtn @click="closeResult" btnName="확인"></cBtn>
+    <p class="total-price">{{ totalPrice }}원</p>
+    <cBtn class="close-btn" @click="closeResult" btnName="확인"></cBtn>
   </section>
 </template>
 
@@ -30,17 +34,65 @@ export default {
 </script>
 
 <style scoped>
+  .black-bg {
+    z-index: 9;
+  }
   .order-result-container {
     position: fixed;
+    margin: 0 auto;
+    left: 0;
+    right: 0;
+    top: 30%;
+    z-index: 10;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
-    left: 10;
-    bottom: 0;
-    z-index: 9;
     width: 90%;
-    height: 30%;
-    background-color: #ccc;
+    height: 40%;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 20px;
+    font-weight: 200;
+  }
+  hr {
+    height: 1px;
+    background-color: var(--green);
+    margin: 10px 0px;
+  }
+  .result-title {
+    font-weight: 500;
+    font-size: 20px;
+    text-align: left;
+    margin-bottom: 10px;
+  }
+  .coffee-info {
+    font-size: 18px;
+    display: flex;
+    text-align: left;
+    justify-content: space-between;
+    margin-bottom: 10px;
+  }
+  .coffee-volume {
+    text-align: right;
+  }
+  .option-info {
+    text-align: left;
+    list-style-type: '- ';
+    padding-left: 20px;
+    line-height: 20px;
+    font-size: 15px;
+  }
+  .total-price {
+    font-weight: 500;
+    font-size: 20px;
+    text-align: right;
+    margin-bottom: 30px;
+  }
+  .close-btn {
+    width: 65px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin: 0px 20px 20px 0px;
   }
 </style>
